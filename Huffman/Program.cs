01c593962328b04fa7e999e069e5287e4c673ddb
Arthur Rozdaybida <makechansongreateagain@gmail.com>
2020-11-28 15:03:53 +0300

    first commit

---
+++ Huffman/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace HuffmanTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the string:");
+            string input = "beep boop beer!";
+            HuffmanTree huffmanTree = new HuffmanTree();
+
+            // Build the Huffman tree
+            huffmanTree.Build(input);
+
+            // Encode
+            BitArray encoded = huffmanTree.Encode(input);
+
+            Console.Write("Encoded: ");
+            foreach (bool bit in encoded)
+            {
+                Console.Write((bit ? 1 : 0) + "");
+            }
+            Console.WriteLine();
+
+            // Decode
+            string decoded = huffmanTree.Decode(encoded);
+
+            Console.WriteLine("Decoded: " + decoded);
+
+            Console.ReadLine();
+        }
+    }
+}
